a20b455413b336f8c7060d4e77f81adc0b7a239c
Denis Ergin <denis.ergin@outlook.de>
2022-06-13 21:37:50 +0200

    Export the FlexContainer via the index file

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export type { ButtonProps } from './button';
+export type { FlexContainerProps } from './flex';
+
 export { default as Button } from './button';
+export { default as FlexContainer } from './flex';
